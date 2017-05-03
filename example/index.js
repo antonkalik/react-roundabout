abdd256bdf15f2d18bd697cbd6aa8c97de078c41
Federico Knüssel <fknussel@gmail.com>
2017-05-03 19:15:48 +1200

    Add first working example

--- example/index.js
+++ example/index.js
@@ -4,11 +4,11 @@ import Roundabout from '../src/index';
 
 const Example = () => (
   <Roundabout loop={true} showNav={false} selected={2}>
-    <div style={{ background: '#21BB9A' }}>1</div>
-    <div style={{ background: '#329ADD' }}>2</div>
-    <div style={{ background: '#9A5CB9' }}>3</div>
-    <div style={{ background: '#E64C3C' }}>4</div>
-    <div style={{ background: '#2D3F52' }}>5</div>
+    <div style={{ background: '#21BB9A', height: '400px' }}>1</div>
+    <div style={{ background: '#329ADD', height: '400px' }}>2</div>
+    <div style={{ background: '#9A5CB9', height: '400px' }}>3</div>
+    <div style={{ background: '#E64C3C', height: '400px' }}>4</div>
+    <div style={{ background: '#2D3F52', height: '400px' }}>5</div>
   </Roundabout>
 );
 
